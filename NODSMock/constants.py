1f44fe56e735636f75d0afd6ce19ae8730183f03
Rafael Direito <rdireito@vpn337.ras.ua.pt>
2025-10-28 09:57:28 +0000

    minor update

--- NODSMock/constants.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: Rafael Direito
-# @Date:   30-05-2022 11:16:25
-# @Email:  rdireito@av.it.pt
-# @Last Modified by:   Rafael Direito
-# @Last Modified time: 2022-10-27 16:03:17
-# @Description: 
-CI_CD_MANAGER_URL = "https://ci-cd-manager.5gasp.eu/manager"
-ONBOARDED_NETAPPS_COUNT = 0
--- build_docker_images.sh
+++
@@ -1,1 +0,0 @@
- docker build --no-cache -f docker/Dockerfile  -t 5gasp:ci_cd_manager  ./
